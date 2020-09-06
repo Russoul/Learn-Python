32bb46a7cb51096eeb648ac7c84c0c6d4f6fdf99
russoul <russoulmc@gmail.com>
2020-09-06 10:23:44 +0300

    Add Homework.py

---
+++ Homework.py
@@ -0,0 +1,14 @@
+
+def xor(x, y) # x ^ y ("исключающее или", одна из 16 булевых функций 2-х аргументов, загугли)
+    pass # Homework
+
+def sumOfDigits(n): # используй рекурсию
+    pass # Homework
+
+def maxDigit(n): # используй рекурсию
+    pass # Homework
+
+# n >= 0
+def isPalendrome(n) # является ли n палиндромом, используй рекурсию
+    pass # Homework
+
