0ca38df48bd7d5a900695ad17f9ba5e2345bd58d
russoul <russoulmc@gmail.com>
2020-09-06 10:25:05 +0300

    Add input restrictions

--- Homework.py
+++ Homework.py
@@ -2,9 +2,11 @@
 def xor(x, y) # x ^ y ("исключающее или", одна из 16 булевых функций 2-х аргументов, загугли)
     pass # Homework
 
+# n >= 0
 def sumOfDigits(n): # используй рекурсию
     pass # Homework
 
+# n >= 0
 def maxDigit(n): # используй рекурсию
     pass # Homework
 
