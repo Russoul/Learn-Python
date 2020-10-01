c3162cfa83bb72f2e98797c1252aa95f5a40eb4e
russoul <russoulmc@gmail.com>
2020-10-01 15:36:47 +0300

    Add homework on lists

--- Lists.py
+++ Lists.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 from typing import *
-from Homework import *
 
 class Nil:
     def __rpow__(self, other):
@@ -49,27 +48,3 @@ def map(f, l): # {x1, x2, x3, ..., xn} -> {f(x1), f(x2), f(x3), ..., f(xn)}
        fx1 = f(l.head)
        t = map(f, l.tail) # f(x1) ** {f(x2), f(x3), ..., f(xn)}
        return fx1 ** t
-
-
-
-l = 1 ** 2 ** 3 ** nil
-r = 0 ** 2 ** 3 ** nil
-
-print(l)
-print(r)
-print(l.tail == r.tail)
-print(isEmpty([]))
-print(isEmpty(()))
-print(isEmpty(nil))
-print(nonEmpty(1 ** nil))
-
-t = 1 ** 2 ** 3 ** nil
-print(t, ":", map(lambda x: (x + 1) ** 2, t))
-
-print("-- ПРОВЕРКА НА ПАЛИНДРОМНОСТЬ --")
-
-def checkPalindromes(l):
-    map(lambda x: print(str(x) + ":" + str(isPalindrome(x))), l)
-
-
-checkPalindromes(111 ** 1 ** 123321 ** 123 ** 12000021 ** nil)
